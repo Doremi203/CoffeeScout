206514b0528a43c4a215001b9817dfbcf531d949
Doremi203 <maxim.r.ivanov@gmail.com>
2024-03-28 00:47:37 +0300

    Modified models

--- Backend/CoffeeScoutBackend.Domain/Models/Customer.cs
+++ Backend/CoffeeScoutBackend.Domain/Models/Customer.cs
@@ -3,5 +3,5 @@ namespace CoffeeScoutBackend.Domain.Models;
 public record Customer
 {
     public string UserId { get; init; } = string.Empty;
-    public required ICollection<MenuItem> FavoriteMenuItems { get; init; }
+    public ICollection<MenuItem> FavoriteMenuItems { get; init; } = new HashSet<MenuItem>();
 }
