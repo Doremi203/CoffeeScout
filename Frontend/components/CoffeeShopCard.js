41de73bc869861cc6beddc99c994cb178b1059b4
mayaffia <maifraimova@edu.hse.ru>
2024-03-28 20:39:31 +0300

    Added route button

--- Frontend/components/CoffeeShopCard.js
+++ Frontend/components/CoffeeShopCard.js
@@ -1,33 +1,148 @@
-import React from 'react';
-import {Image, StyleSheet, Text, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {
+    ActionSheetIOS,
+    Alert,
+    Image,
+    Linking, Platform,
+    StyleSheet,
+    Text,
+    TouchableWithoutFeedback,
+    View
+} from 'react-native';
 import {RFPercentage, RFValue} from "react-native-responsive-fontsize";
+import * as Location from 'expo-location';
 
+const getUrl = () => {
+    const [location, setLocation] = useState(null);
+    const [url, setUrl] = useState("");
+
+    useEffect(() => {
+        (async () => {
+            let {status} = await Location.requestForegroundPermissionsAsync();
+            if (status !== 'granted') {
+                Alert.alert('Permission to access location was denied');
+                return;
+            }
+
+            let location = await Location.getCurrentPositionAsync({});
+            setLocation(location);
+
+            console.log(location.coords.longitude)
+            console.log(location.coords.latitude)
+
+            if (location) {
+                setUrl("https://yandex.ru/maps/213/moscow/?indoorLevel=1&ll=" + location.coords.longitude + "%2C" + location.coords.latitude + "&mode=routes&rtext=55.697182%2C37.500930~55.698969%2C37.499201&rtt=pd&ruri=~&z=17");
+            }
+        })();
+    }, []);
+
+    console.log(url)
+
+    return url
+};
+
+
+//const supportedURL = "https://www.google.com/maps/dir/?api=1&destination=55.697695,37.497517\n";
+//const supportedURL = "https://yandex.ru/maps/org/1723336082\n";
+const supportedURL = "https://yandex.ru/maps/?mode=routes&rtext=~55.698969%2C37.499201&rtt=pd\n\n";
+
+
+//<OpenURLButton url={url}></OpenURLButton>
 
 export default function CoffeeShopCard({navigation}) {
+
+    const urlYandex = getUrl();
+    const urlGoogle = "https://www.google.com/maps/dir/?api=1&destination=55.697695,37.497517\n";
+
+
+    const onPress = () => {
+        if (Platform.OS === 'ios') {
+            ActionSheetIOS.showActionSheetWithOptions(
+                {
+                    options: ['Cancel', 'Яндекс карты', 'Google maps'],
+                    // destructiveButtonIndex: 2,
+                    cancelButtonIndex: 0,
+                    userInterfaceStyle: 'dark',
+                },
+                async buttonIndex => {
+                    if (buttonIndex === 0) {
+                        // cancel action
+                    } else if (buttonIndex === 1) {
+                        const supported = await Linking.canOpenURL(urlYandex);
+                        if (supported) {
+                            await Linking.openURL(urlYandex);
+                        } else {
+                            Alert.alert("Dont know how to open this URL:");
+                        }
+                    } else if (buttonIndex === 2) {
+                        const supported = await Linking.canOpenURL(urlGoogle);
+                        if (supported) {
+                            await Linking.openURL(urlGoogle);
+                        } else {
+                            Alert.alert("Dont know how to open this URL:");
+                        }
+                    }
+                },
+            );
+        } else {
+            Alert.alert(
+                'Choose Map App',
+                'Which map application would you like to use?',
+                [
+                    {
+                        text: 'Cancel',
+                        style: 'cancel',
+                    },
+                    {
+                        text: 'Yandex Maps',
+                        onPress: async () => {
+                            const supported = await Linking.canOpenURL(urlYandex);
+                            if (supported) {
+                                await Linking.openURL(urlYandex);
+                            } else {
+                                Alert.alert("Don't know how to open this URL:");
+                            }
+                        },
+                    },
+                    {
+                        text: 'Google Maps',
+                        onPress: async () => {
+                            const supported = await Linking.canOpenURL(urlGoogle);
+                            if (supported) {
+                                await Linking.openURL(urlGoogle);
+                            } else {
+                                Alert.alert("Don't know how to open this URL:");
+                            }
+                        },
+                    },
+                ],
+                {cancelable: false}
+            );
+        }
+    }
+
     return (
         <View style={styles.square}>
-            <Image source={require('../assets/icons/coffee.png')} style={styles.image} />
+            <Image source={require('../assets/icons/coffee.png')} style={styles.image}/>
             <Text style={styles.label}>One Price Coffee</Text>
 
-
-            <View style={styles.routeButton}>
-                <Text style={styles.routeText}> МАРШРУТ </Text>
-            </View>
-
+            <TouchableWithoutFeedback onPress={onPress}>
+                <View style={styles.routeButton}>
+                    <Text style={styles.routeText}> МАРШРУТ </Text>
+                </View>
+            </TouchableWithoutFeedback>
 
 
         </View>
     );
 };
 
+
 const styles = StyleSheet.create({
-    container: {
-    },
+    container: {},
     square: {
         marginVertical: 10,
         flexDirection: 'row',
-        // width: windowWidth * 0.85,
-        // height: windowHeight* 0.15,
         width: RFValue(300),
         height: RFValue(115),
         backgroundColor: 'white',
@@ -42,14 +157,13 @@ const styles = StyleSheet.create({
         elevation: 5,
     },
 
-    routeButton : {
-        borderColor:'black',
-        borderWidth:1,
-        //backgroundColor:'red',
+    routeButton: {
+        borderColor: 'black',
+        borderWidth: 1,
         width: RFValue(150),
         height: RFValue(40),
-        borderRadius:20,
-        top:'20%',
+        borderRadius: 20,
+        top: '20%',
         right: RFValue(130),
 
     },
@@ -58,7 +172,6 @@ const styles = StyleSheet.create({
         marginTop: RFValue(11),
         marginLeft: RFValue(32),
         fontFamily: 'MontserratAlternates',
-        //top:'60%'
     },
     image: {
         marginLeft: 20,
@@ -75,5 +188,3 @@ const styles = StyleSheet.create({
     },
 
 });
-
-
