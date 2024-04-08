4c79575ebcf701b2a6119b404330514ee0fa6810
Ivanov Maxim Romanovich <32964154+Doremi203@users.noreply.github.com>
2024-04-08 17:46:07 +0300

    Remove favorite item endpoint
    * Implemented remove favorite item endpoint

--- Backend/CoffeeScoutBackend.Domain/Interfaces/Services/ICustomerService.cs
+++ Backend/CoffeeScoutBackend.Domain/Interfaces/Services/ICustomerService.cs
@@ -6,7 +6,8 @@ public interface ICustomerService
 {
     Task RegisterCustomer(CustomerRegistrationData customerRegistrationData);
     Task<Customer> GetByUserId(string userId);
-    Task AddFavoredMenuItem(string currentUserId, long menuItemId);
+    Task AddFavoredMenuItem(string userId, long menuItemId);
+    Task RemoveFavoredMenuItem(string userId, long menuItemId);
     Task<IReadOnlyCollection<BeverageType>> GetFavoredBeverageTypes(string userId);
     Task<CustomerInfo> GetInfo(string userId);
     Task UpdateInfo(string userId, CustomerInfo info);
