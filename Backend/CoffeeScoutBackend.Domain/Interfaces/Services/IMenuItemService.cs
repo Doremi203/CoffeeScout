13e268398e7096f0c6d280b030b5890a57fc23db
Doremi203 <maxim.r.ivanov@gmail.com>
2024-04-13 02:14:10 +0300

    Added get menu items of cafe for customer

--- Backend/CoffeeScoutBackend.Domain/Interfaces/Services/IMenuItemService.cs
+++ Backend/CoffeeScoutBackend.Domain/Interfaces/Services/IMenuItemService.cs
@@ -5,10 +5,11 @@ namespace CoffeeScoutBackend.Domain.Interfaces.Services;
 public interface IMenuItemService
 {
     Task<MenuItem> GetById(long id);
+    Task<IReadOnlyCollection<MenuItem>> GetByCafeId(long id);
     Task<IReadOnlyCollection<MenuItem>> GetAllInAreaByBeverageType(
         Location location, double radiusInMeters, long beverageTypeId);
     Task<MenuItem> Add(string cafeAdminId, MenuItem menuItem);
     Task Update(long id, MenuItem menuItem);
-    Task<IReadOnlyCollection<MenuItem>> GetCafeMenuItems(string cafeAdminId);
+    Task<IReadOnlyCollection<MenuItem>> GetByCafeAdmin(string cafeAdminId);
     Task Delete(long id);
 }
