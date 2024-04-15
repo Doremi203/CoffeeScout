d41516f8c5437c79dc5592f1527f13b8b40b29b5
Doremi203 <maxim.r.ivanov@gmail.com>
2024-04-16 02:58:18 +0300

    Refactored beverage type responses

--- Backend/CoffeeScoutBackend.Api/Controllers/BeverageTypesController.cs
+++ Backend/CoffeeScoutBackend.Api/Controllers/BeverageTypesController.cs
@@ -1,5 +1,6 @@
 using CoffeeScoutBackend.Api.Requests.V1.Beverages;
 using CoffeeScoutBackend.Api.Responses;
+using CoffeeScoutBackend.Api.Responses.V1.Beverages;
 using CoffeeScoutBackend.Domain.Interfaces.Services;
 using CoffeeScoutBackend.Domain.Models;
 using Mapster;
@@ -16,23 +17,23 @@ public class BeverageTypesController(
 {
     [HttpPost]
     [Authorize(Roles = nameof(Roles.SuperAdmin))]
-    [ProducesResponseType<BeverageTypeResponse>(StatusCodes.Status201Created)]
+    [ProducesResponseType<AddBeverageTypeResponse>(StatusCodes.Status201Created)]
     public async Task<IActionResult> AddBeverageType(AddBeverageTypeRequest request)
     {
         var addedBeverageType = 
             await beverageTypeService.Add(request.Adapt<BeverageType>());
 
-        return Created($"{RoutesV1.BeverageTypes}/{addedBeverageType.Id}", addedBeverageType.Adapt<BeverageTypeResponse>());
+        return Created($"{RoutesV1.BeverageTypes}/{addedBeverageType.Id}", addedBeverageType.Adapt<AddBeverageTypeResponse>());
     }
     
     [HttpGet]
     [Authorize(Roles = $"{nameof(Roles.SuperAdmin)},{nameof(Roles.CafeAdmin)},{nameof(Roles.Customer)}")]
-    [ProducesResponseType<IReadOnlyCollection<BeverageTypeResponse>>(StatusCodes.Status200OK)]
+    [ProducesResponseType<IReadOnlyCollection<GetBeverageTypeResponse>>(StatusCodes.Status200OK)]
     public async Task<IActionResult> GetBeverageTypes([FromQuery] GetBeverageTypesRequest request)
     {
         var beverageTypes = await beverageTypeService.GetPage(request.PageSize, request.PageNumber);
 
-        return Ok(beverageTypes.Adapt<IReadOnlyCollection<BeverageTypeResponse>>());
+        return Ok(beverageTypes.Adapt<IReadOnlyCollection<GetBeverageTypeResponse>>());
     }
 
     [HttpPatch("{id:long}")]
