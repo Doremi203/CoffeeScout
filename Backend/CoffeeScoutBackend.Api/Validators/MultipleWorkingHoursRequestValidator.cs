5b1caf100ef67fc65cbf863519009a50c2a2881a
Doremi203 <maxim.r.ivanov@gmail.com>
2024-04-16 19:17:32 +0300

    Added validation for cafe requests

---
+++ Backend/CoffeeScoutBackend.Api/Validators/MultipleWorkingHoursRequestValidator.cs
@@ -0,0 +1,21 @@
+using CoffeeScoutBackend.Api.Requests.V1.Cafes;
+using FluentValidation;
+
+namespace CoffeeScoutBackend.Api.Validators;
+
+public class MultipleWorkingHoursRequestValidator : AbstractValidator<WorkingHoursRequest[]>
+{
+    public MultipleWorkingHoursRequestValidator(IValidator<WorkingHoursRequest> workingHoursValidator)
+    {
+        var days = new HashSet<DayOfWeek>();
+        RuleFor(request => request)
+            .Must(workingHours =>
+                workingHours.Length == 7 &&
+                workingHours
+                    .All(wh => days.Add(wh.DayOfWeek)))
+            .WithMessage("Working hours must be provided for all days of the week.");
+
+        RuleForEach(request => request)
+            .SetValidator(workingHoursValidator);
+    }
+}
