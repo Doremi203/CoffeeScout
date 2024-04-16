d917256bfcfdf9b274c6bbd8d19939512a78752c
Ivanov Maxim Romanovich <32964154+Doremi203@users.noreply.github.com>
2024-04-17 00:21:55 +0300

    Account requests validation
    * Added account requests validation

---
+++ Backend/CoffeeScoutBackend.Api/Validators/Accounts/RegisterCafeAdminRequestValidator.cs
@@ -0,0 +1,16 @@
+using CoffeeScoutBackend.Api.Requests.V1.Accounts;
+using FluentValidation;
+
+namespace CoffeeScoutBackend.Api.Validators.Accounts;
+
+public class RegisterCafeAdminRequestValidator : AbstractValidator<RegisterCafeAdminRequest>
+{
+    public RegisterCafeAdminRequestValidator()
+    {
+        RuleFor(request => request.Email)
+            .NotEmpty()
+            .EmailAddress();
+        RuleFor(request => request.Password)
+            .NotEmpty();
+    }
+}
---
+++ Backend/CoffeeScoutBackend.Api/Validators/Accounts/RegisterCustomerRequestValidator.cs
@@ -0,0 +1,18 @@
+using CoffeeScoutBackend.Api.Requests.V1.Accounts;
+using FluentValidation;
+
+namespace CoffeeScoutBackend.Api.Validators.Accounts;
+
+public class RegisterCustomerRequestValidator : AbstractValidator<RegisterCustomerRequest>
+{
+    public RegisterCustomerRequestValidator()
+    {
+        RuleFor(request => request.FirstName)
+            .NotEmpty();
+        RuleFor(request => request.Email)
+            .NotEmpty()
+            .EmailAddress();
+        RuleFor(request => request.Password)
+            .NotEmpty();
+    }
+}
