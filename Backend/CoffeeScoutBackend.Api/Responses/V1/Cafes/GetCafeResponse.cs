811e867abbe2d9b5d2a8760bfb78cff17fe75565
Doremi203 <maxim.r.ivanov@gmail.com>
2024-04-16 14:51:09 +0300

    fixed cafe responses for swagger

--- Backend/CoffeeScoutBackend.Api/Responses/V1/Cafes/GetCafeResponse.cs
+++ Backend/CoffeeScoutBackend.Api/Responses/V1/Cafes/GetCafeResponse.cs
@@ -10,17 +10,4 @@ public class GetCafeResponse
     public required Location Location { get; set; }
     public required string Address { get; set; }
     public required IReadOnlyCollection<WorkingHoursResponse> WorkingHours { get; set; }
-
-    public record WorkingHoursResponse
-    {
-        public required DayOfWeek DayOfWeek { get; set; }
-        public required TimeResponse OpeningTime { get; set; }
-        public required TimeResponse ClosingTime { get; set; }
-
-        public record TimeResponse
-        {
-            public required int Hour { get; set; }
-            public required int Minute { get; set; }
-        }
-    }
 }
