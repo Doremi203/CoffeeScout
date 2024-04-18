b8017143db1d8510f27e13ccaa7bdf6fc96406fb
mayaffia <maifraimova@edu.hse.ru>
2024-04-18 15:33:13 +0300

    Refactored cafe screen

--- Frontend/mobile/services/CafeService.js
+++ Frontend/mobile/services/CafeService.js
@@ -4,7 +4,7 @@ import $api from "../http";
 export default class CafeService {
 
     static async getNearbyCafes(longitude, latitude, radius) {
-        return $api.get(`/v1/cafes?Latitude=${latitude}&Longitude=${longitude}&RadiusInMeters=${radius}`)
+        return $api.get(`/v1/cafes?Location.Latitude=${latitude}&Location.Longitude=${longitude}&RadiusInMeters=${radius}`)
     }
 
     static async getMenu(cafeId) {
