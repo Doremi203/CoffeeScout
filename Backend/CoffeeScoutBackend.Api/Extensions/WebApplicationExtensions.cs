912ce217a2119ce95ed244cd6bc7dc527748c9b2
Ivanov Maxim Romanovich <32964154+Doremi203@users.noreply.github.com>
2024-04-21 20:04:50 +0300

    Added exception handling
    * Added exception handling

---
+++ Backend/CoffeeScoutBackend.Api/Extensions/WebApplicationExtensions.cs
@@ -0,0 +1,16 @@
+using CoffeeScoutBackend.Api.Middlewares;
+
+namespace CoffeeScoutBackend.Api.Extensions;
+
+public static class WebApplicationExtensions
+{
+    public static WebApplication AddExceptionHandlingMiddlewares(this WebApplication app)
+    {
+        app.UseExceptionHandler();
+        app.UseMiddleware<NotFoundExceptionHandlingMiddleware>();
+        app.UseMiddleware<InvalidOrderDataExceptionHandlingMiddleware>();
+        app.UseMiddleware<InvalidOrderStatusExceptionHandlingMiddleware>();
+        app.UseMiddleware<PaymentExceptionHandlingMiddleware>();
+        return app;
+    }
+}
--- Backend/CoffeeScoutBackend.Api/Program.cs
+++ Backend/CoffeeScoutBackend.Api/Program.cs
@@ -1,7 +1,7 @@
 using CoffeeScoutBackend.Api;
 using CoffeeScoutBackend.Api.Config;
-using CoffeeScoutBackend.Api.Config.Swager;
 using CoffeeScoutBackend.Api.DbSeeders;
+using CoffeeScoutBackend.Api.Extensions;
 using CoffeeScoutBackend.Api.Identity;
 using CoffeeScoutBackend.Api.Requests.Mappers;
 using CoffeeScoutBackend.Api.Responses.Mappers;
@@ -12,9 +12,7 @@ using CoffeeScoutBackend.Dal.Entities;
 using FluentValidation;
 using MailerSendNetCore.Common.Extensions;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.OpenApi.Models;
 using SharpGrip.FluentValidation.AutoValidation.Mvc.Extensions;
-using Swashbuckle.AspNetCore.Filters;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -30,21 +28,9 @@ ResponseMapperConfiguration.Configure();
 RequestMapperConfiguration.Configure();
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(options =>
-    {
-        options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
-        {
-            In = ParameterLocation.Header,
-            Name = "Authorization",
-            Type = SecuritySchemeType.ApiKey
-        });
-        options.OperationFilter<SecurityRequirementsOperationFilter>();
-        options.OperationFilter<AuthorizeCheckOperationFilter>();
-
-        options.DocumentFilter<RemovePathsDocumentFilter>();
-    }
-);
+builder.Services.AddSwagger();
 builder.Services.AddControllers();
+builder.Services.AddProblemDetails();
 
 builder.Services.AddAuthentication();
 builder.Services.AddAuthorizationBuilder();
@@ -90,7 +76,9 @@ app
     .WithTags("Accounts")
     .MapIdentityApi<AppUser>();
 
-app.UseAuthorization();
+//app.UseAuthorization();
+
+app.AddExceptionHandlingMiddlewares();
 
 app.MapControllers();
 
